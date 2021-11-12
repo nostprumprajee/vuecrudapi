94b6217fbfa9772a66cada9a6409d9c2f792506d
Wirawat.B <nostprumprajee@gmail.com>
2021-11-12 16:33:34 +0700

    add taskRoutes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const taskBuilder = require('../controllers/taskController')
+
+module.exports = app => {
+    app
+        .route('/task')
+        .get(taskBuilder.list_all_tasks)
+        .post(taskBuilder.create_a_task);
+    app
+        .route('/task/:taskId')
+        .get(taskBuilder.read_a_task)
+        .put(taskBuilder.put_a_task)
+        .delete(taskBuilder.delete_a_task);
+}
